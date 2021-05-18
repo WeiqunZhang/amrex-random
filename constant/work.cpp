172ec275702612542971f8816d5d74d5fb525aab
Weiqun Zhang <weiqunzhang@lbl.gov>
2021-05-18 09:54:48 -0700

    update constant memory test

--- constant/work.cpp
+++ constant/work.cpp
@@ -4,7 +4,7 @@
 
 void work ()
 {
-    amrex::Gpu::PinnedVector<int> pv(4);
+    amrex::Gpu::PinnedVector<int> pv(5);
     int* ppv = pv.data();
 
     amrex::ParallelFor(1, [=] AMREX_GPU_DEVICE (int)
@@ -13,9 +13,11 @@ void work ()
         ppv[1] = d_b[0];
         ppv[2] = d_b[1];
         ppv[3] = d_b[2];
+        ppv[4] = d_b[3];
     });
 
     amrex::Gpu::synchronize();
 
-    amrex::Print() << pv[0] << ", " << pv[1] << ", " << pv[2] << ", " << pv[3] << std::endl;
+    amrex::Print() << pv[0] << ", " << pv[1] << ", " << pv[2]
+                   << ", " << pv[3] << ", " << ppv[4] << std::endl;
 }
